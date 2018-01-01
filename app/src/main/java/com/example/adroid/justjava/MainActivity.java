3eb5d4d87275a63fd99e18404ab3d95379b29531
John Hancock <jhancock1975@gmail.com>
2017-12-31 22:42:56 -0500

    open e-mail intent when order button pressed

--- app/src/main/java/com/example/adroid/justjava/MainActivity.java
+++ app/src/main/java/com/example/adroid/justjava/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.adroid.justjava;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -16,7 +18,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private static final int PRICE = 5;
-    int qty = 99;
+    int qty = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,10 +43,10 @@ public class MainActivity extends AppCompatActivity {
         Log.d(this.getClass().getSimpleName(), "user_name =" + userName);
 
 
-        String priceMessage = createOrderSummary(calculatePrice(addWhippedCream, addChocolate),
+        createOrderSummary(calculatePrice(addWhippedCream, addChocolate),
                 addWhippedCream, addChocolate, userName);
 
-        displayMessage(priceMessage);
+        //displayMessage(priceMessage);
     }
 
     /**
@@ -58,15 +60,24 @@ public class MainActivity extends AppCompatActivity {
      * @param userName - name the user entered at the top of the order form
      * @return - string containing the order summary
      */
-    private String createOrderSummary(int price, boolean addWhippedCream,
+    private void createOrderSummary(int price, boolean addWhippedCream,
                                       boolean addChocolate, String userName) {
 
-        return "Name: " + userName
+        String eMailMsg = "Name: " + userName
                 + "\nAdd whipped cream? " + addWhippedCream
                 + "\nAdd chocolate? " + addChocolate
                 + "\nQuantity: " + qty
                 + "\nTotal: $" + price
                 + "\nThank you!";
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setType("*/*");
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_SUBJECT, "JustJava order for: " + userName);
+        intent.putExtra(Intent.EXTRA_TEXT, eMailMsg);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     /**
